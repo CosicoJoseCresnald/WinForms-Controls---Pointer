0fde3b22909277190ff9aca4d9609deb90de5012
Jose Cosico <162939396+CosicoJoseCresnald@users.noreply.github.com>
2024-04-21 17:16:44 +0800

    Add files via upload

---
+++ WinForms Controls - Pointers/WinForms Controls - Pointers1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WinForms_Controls___Pointers1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pointersLbl = new Label();
+            imagePointerBtn = new Button();
+            helpPointerBtn = new Button();
+            customPointerBtn = new Button();
+            defaultPointerBtn = new Button();
+            handPointerBtn = new Button();
+            changingMousePointerLbl = new Label();
+            switchPointerPosition1 = new Button();
+            switchPointerPosition2 = new Button();
+            controllingPointerLbl = new Label();
+            hidePointerBtn = new Button();
+            pointerAccessLbl = new Label();
+            SuspendLayout();
+            // 
+            // pointersLbl
+            // 
+            pointersLbl.AutoSize = true;
+            pointersLbl.Font = new Font("Century Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            pointersLbl.ForeColor = SystemColors.ControlText;
+            pointersLbl.Location = new Point(473, 38);
+            pointersLbl.Name = "pointersLbl";
+            pointersLbl.Size = new Size(123, 30);
+            pointersLbl.TabIndex = 36;
+            pointersLbl.Text = "POINTERS";
+            // 
+            // imagePointerBtn
+            // 
+            imagePointerBtn.BackColor = SystemColors.Info;
+            imagePointerBtn.FlatAppearance.BorderColor = Color.Black;
+            imagePointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            imagePointerBtn.Location = new Point(761, 465);
+            imagePointerBtn.Name = "imagePointerBtn";
+            imagePointerBtn.Size = new Size(195, 74);
+            imagePointerBtn.TabIndex = 35;
+            imagePointerBtn.Text = "CHANGE TO IMAGE POINTER";
+            imagePointerBtn.UseVisualStyleBackColor = false;
+            imagePointerBtn.Click += imagePointerBtn_Click;
+            // 
+            // helpPointerBtn
+            // 
+            helpPointerBtn.BackColor = SystemColors.Info;
+            helpPointerBtn.FlatAppearance.BorderColor = Color.Black;
+            helpPointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            helpPointerBtn.Location = new Point(761, 305);
+            helpPointerBtn.Name = "helpPointerBtn";
+            helpPointerBtn.Size = new Size(195, 74);
+            helpPointerBtn.TabIndex = 34;
+            helpPointerBtn.Text = "CHANGE TO HELP POINTER";
+            helpPointerBtn.UseVisualStyleBackColor = false;
+            helpPointerBtn.Click += helpPointerBtn_Click_1;
+            // 
+            // customPointerBtn
+            // 
+            customPointerBtn.BackColor = SystemColors.Info;
+            customPointerBtn.FlatAppearance.BorderColor = Color.Black;
+            customPointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            customPointerBtn.Location = new Point(761, 385);
+            customPointerBtn.Name = "customPointerBtn";
+            customPointerBtn.Size = new Size(195, 74);
+            customPointerBtn.TabIndex = 33;
+            customPointerBtn.Text = "CHANGE TO CUSTOM POINTER";
+            customPointerBtn.UseVisualStyleBackColor = false;
+            customPointerBtn.Click += customPointerBtn_Click_1;
+            // 
+            // defaultPointerBtn
+            // 
+            defaultPointerBtn.BackColor = SystemColors.Info;
+            defaultPointerBtn.FlatAppearance.BorderColor = Color.Black;
+            defaultPointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            defaultPointerBtn.Location = new Point(761, 159);
+            defaultPointerBtn.Name = "defaultPointerBtn";
+            defaultPointerBtn.Size = new Size(195, 60);
+            defaultPointerBtn.TabIndex = 32;
+            defaultPointerBtn.Text = "CHANGE TO DEFAULT";
+            defaultPointerBtn.UseVisualStyleBackColor = false;
+            defaultPointerBtn.Click += defaultPointerBtn_Click;
+            // 
+            // handPointerBtn
+            // 
+            handPointerBtn.BackColor = SystemColors.Info;
+            handPointerBtn.FlatAppearance.BorderColor = Color.Black;
+            handPointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            handPointerBtn.Location = new Point(761, 225);
+            handPointerBtn.Name = "handPointerBtn";
+            handPointerBtn.Size = new Size(195, 74);
+            handPointerBtn.TabIndex = 31;
+            handPointerBtn.Text = "CHANGE TO HAND POINTER";
+            handPointerBtn.UseVisualStyleBackColor = false;
+            handPointerBtn.Click += handPointerBtn_Click_1;
+            // 
+            // changingMousePointerLbl
+            // 
+            changingMousePointerLbl.AutoSize = true;
+            changingMousePointerLbl.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            changingMousePointerLbl.Location = new Point(738, 105);
+            changingMousePointerLbl.Name = "changingMousePointerLbl";
+            changingMousePointerLbl.Size = new Size(238, 22);
+            changingMousePointerLbl.TabIndex = 30;
+            changingMousePointerLbl.Text = "CHANGING THE POINTER";
+            // 
+            // switchPointerPosition1
+            // 
+            switchPointerPosition1.BackColor = SystemColors.Info;
+            switchPointerPosition1.FlatAppearance.BorderColor = Color.Black;
+            switchPointerPosition1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            switchPointerPosition1.Location = new Point(433, 175);
+            switchPointerPosition1.Name = "switchPointerPosition1";
+            switchPointerPosition1.Size = new Size(195, 60);
+            switchPointerPosition1.TabIndex = 29;
+            switchPointerPosition1.Text = "SWITCH POINTER POSITION (1)";
+            switchPointerPosition1.UseVisualStyleBackColor = false;
+            switchPointerPosition1.Click += switchPointerPosition1_Click_1;
+            // 
+            // switchPointerPosition2
+            // 
+            switchPointerPosition2.BackColor = SystemColors.Info;
+            switchPointerPosition2.FlatAppearance.BorderColor = Color.Black;
+            switchPointerPosition2.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            switchPointerPosition2.Location = new Point(433, 273);
+            switchPointerPosition2.Name = "switchPointerPosition2";
+            switchPointerPosition2.Size = new Size(195, 74);
+            switchPointerPosition2.TabIndex = 28;
+            switchPointerPosition2.Text = "SWITCH POINTER POSITION (2)\r\n";
+            switchPointerPosition2.UseVisualStyleBackColor = false;
+            switchPointerPosition2.Click += switchPointerPosition2_Click_1;
+            // 
+            // controllingPointerLbl
+            // 
+            controllingPointerLbl.AutoSize = true;
+            controllingPointerLbl.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            controllingPointerLbl.Location = new Point(400, 105);
+            controllingPointerLbl.Name = "controllingPointerLbl";
+            controllingPointerLbl.Size = new Size(265, 22);
+            controllingPointerLbl.TabIndex = 27;
+            controllingPointerLbl.Text = "CONTROLLING THE POINTER";
+            // 
+            // hidePointerBtn
+            // 
+            hidePointerBtn.BackColor = SystemColors.Info;
+            hidePointerBtn.FlatAppearance.BorderColor = Color.Black;
+            hidePointerBtn.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            hidePointerBtn.Location = new Point(92, 175);
+            hidePointerBtn.Name = "hidePointerBtn";
+            hidePointerBtn.Size = new Size(219, 89);
+            hidePointerBtn.TabIndex = 26;
+            hidePointerBtn.Text = "HIDE POINTER";
+            hidePointerBtn.UseVisualStyleBackColor = false;
+            hidePointerBtn.MouseEnter += hidePointerBtn_MouseEnter;
+            hidePointerBtn.MouseLeave += hidePointerBtn_MouseLeave;
+            // 
+            // pointerAccessLbl
+            // 
+            pointerAccessLbl.AutoSize = true;
+            pointerAccessLbl.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            pointerAccessLbl.Location = new Point(115, 105);
+            pointerAccessLbl.Name = "pointerAccessLbl";
+            pointerAccessLbl.Size = new Size(165, 22);
+            pointerAccessLbl.TabIndex = 25;
+            pointerAccessLbl.Text = "POINTER ACCESS";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1069, 577);
+            Controls.Add(pointersLbl);
+            Controls.Add(imagePointerBtn);
+            Controls.Add(helpPointerBtn);
+            Controls.Add(customPointerBtn);
+            Controls.Add(defaultPointerBtn);
+            Controls.Add(handPointerBtn);
+            Controls.Add(changingMousePointerLbl);
+            Controls.Add(switchPointerPosition1);
+            Controls.Add(switchPointerPosition2);
+            Controls.Add(controllingPointerLbl);
+            Controls.Add(hidePointerBtn);
+            Controls.Add(pointerAccessLbl);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "WinForms Controls - Pointers";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label pointersLbl;
+        private Button imagePointerBtn;
+        private Button helpPointerBtn;
+        private Button customPointerBtn;
+        private Button defaultPointerBtn;
+        private Button handPointerBtn;
+        private Label changingMousePointerLbl;
+        private Button switchPointerPosition1;
+        private Button switchPointerPosition2;
+        private Label controllingPointerLbl;
+        private Button hidePointerBtn;
+        private Label pointerAccessLbl;
+    }
+}
---
+++ WinForms Controls - Pointers/WinForms Controls - Pointers1/Form1.cs
@@ -0,0 +1,99 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Devices;
+using System.Reflection;
+
+namespace WinForms_Controls___Pointers1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            defaultPointerBtn.Cursor = Cursors.Default;
+            handPointerBtn.Cursor = Cursors.Hand;
+            helpPointerBtn.Cursor = Cursors.Help;
+            customPointerBtn.Cursor = new Cursor("CustomCursor.cur");
+        }
+        //WINDOWS FORMS CONTROLS - POINTERS
+        /*
+         Mouse pointer, or cursor is used to navigate to the computer.
+         Controlled by a device, such as mouse ,tablet or trackball,
+         it is also used to point on where the user would like to click in
+         order to interact with it.Aside from the default pointers,
+         there are other pointers that are used in order to specify
+         on what interaction does an object implies.
+
+
+        All of the controls have a cursor property
+        Custom cursors must be on a .cur file format
+        */
+
+        //POINTER ACCESS
+        //HIDES THE POINTER WHEN MOUSE ENTERS/LEAVES THE CONTROL
+        private void hidePointerBtn_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void hidePointerBtn_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+
+        //CHANGE POINTER POSITION
+        private void switchPointerPosition1_Click_1(object sender, EventArgs e)
+        {
+            //moves the cursor into a certain control
+            Cursor.Position = PointToScreen(switchPointerPosition2.Location);
+            //moves the pointer into a certain coordinate shonw in the screen
+            //Cursor.Position = new Point(325, 600);
+        }
+
+        private void switchPointerPosition2_Click_1(object sender, EventArgs e)
+        {
+            Cursor.Position = PointToScreen(switchPointerPosition1.Location);
+        }
+
+        //CHANGE POINTER
+        //default
+        private void defaultPointerBtn_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+        //hand
+        private void handPointerBtn_Click_1(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+        //help
+        private void helpPointerBtn_Click_1(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Help;
+        }
+        //custom cursor
+        private void customPointerBtn_Click_1(object sender, EventArgs e)
+        {
+            Cursor = new Cursor("CustomCursor.cur");
+        }
+        //image cursor
+        private void imagePointerBtn_Click(object sender, EventArgs e)
+        {
+            //file location path may vary
+            //only jpg files can be used for image format
+            Bitmap bitmap1 = new Bitmap(new Bitmap("F:\\vsc codes\\WinForms Controls - Pointers\\WinForms Controls - Pointers1\\image.jpg"), 70, 70);
+            imagePointerBtn.Cursor = new Cursor(bitmap1.GetHicon());
+            Cursor = new Cursor(bitmap1.GetHicon());
+        }
+
+        //REFERENCES
+        // Cursor Class - https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor?view=windowsdesktop-8.0
+        // How to Manage Mouse Pointer - https://learn.microsoft.com/en-us/dotnet/desktop/winforms/input-mouse/how-to-manage-cursor-pointer?view=netdesktop-8.0#changing-the-mouse-pointer
+        // Pointer - C# Windows Forms Control - https://youtu.be/zKb79hEqaAw?si=w9m-WMlIuGU0kZWp 
+        // C# move mouse cursor - https://stackoverflow.com/questions/55061384/c-sharp-move-mouse-cursor-to-a-specific-position-on-screen-for-windows-10-64bit
+    }
+}
